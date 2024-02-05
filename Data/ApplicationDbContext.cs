87aee6cb5dc494ff96649f8d142bbda4c2ba5b50
Kamil Nowakowski <Nowakooo26x@gmail.com>
2024-02-05 12:37:09 +0100

    Dodaj pliki projektów.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,254 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AppModel.Models;
+
+namespace DataBaseComplete
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Book_Author> Book_Author { get; set; }
+        public DbSet<Publisher> Publisher { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Relations
+            // Many-to-Many: Book and Author
+            modelBuilder.Entity<Book_Author>()
+                .HasKey(ba => new { ba.BookId, ba.AuthorId });
+
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(ba => ba.Book)
+                .WithMany(b => b.BookAuthors)
+                .HasForeignKey(ba => ba.BookId);
+
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(ba => ba.Author)
+                .WithMany(a => a.BookAuthors)
+                .HasForeignKey(ba => ba.AuthorId);
+            // One-to-Many: Book and Publisher
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Publisher)
+                .WithMany(p => p.Books)
+                .HasForeignKey(b => b.PublisherId);
+
+            //ObjectCreating
+            //Authors
+            modelBuilder.Entity<Author>()
+               .HasData(
+                    new Author()
+                    {
+                        AuthorId = 1,
+                        FirstName = "Stanisław",
+                        LastName = "Lem",
+                        DateOfBirth = new DateTime(1921, 9, 12),
+                        CountryOfOrigin = "Poland",
+                    },
+                    new Author()
+                    {
+                        AuthorId = 2,
+                        FirstName = "Robert C.",
+                        LastName = "Martin",
+                        DateOfBirth = new DateTime(1952, 12, 5),
+                        CountryOfOrigin = "America",
+                    },
+                    new Author()
+                    {
+                        AuthorId = 3,
+                        FirstName = "Erich",
+                        LastName = "Gamma",
+                        DateOfBirth = new DateTime(1961, 3, 13),
+                        CountryOfOrigin = "Switzerland",
+                    },
+                    new Author()
+                    {
+                        AuthorId = 4,
+                        FirstName = "Richard",
+                        LastName = "Helm",
+                        DateOfBirth = new DateTime(9999, 1, 1),
+                        CountryOfOrigin = ""
+                    },
+                    new Author()
+                    {
+                        AuthorId = 5,
+                        FirstName = "Ralph",
+                        LastName = "Johnson",
+                        DateOfBirth = new DateTime(9999, 1, 1),
+                        CountryOfOrigin = "",
+                    },
+                    new Author()
+                    {
+                        AuthorId = 6,
+                        FirstName = "John",
+                        LastName = "Vlissides",
+                        DateOfBirth = new DateTime(1961, 8, 2),
+                        CountryOfOrigin = "America",
+                    },
+                    new Author()
+                    {
+                        AuthorId = 7,
+                        FirstName = "Walter",
+                        LastName = "Isaacson",
+                        DateOfBirth = new DateTime(1952, 3, 20),
+                        CountryOfOrigin = "America",
+                    }
+                );
+            //Publishers
+            modelBuilder.Entity<Publisher>()
+                .HasData(
+                    new Publisher()
+                    {
+                        PublisherId = 1,
+                        Name = "Wydawnictwo Literackie",
+                        Address = "Długa 1, 31-147 Kraków",
+                        YearFounded = 1953,
+                    },
+                    new Publisher()
+                    {
+                        PublisherId = 2,
+                        Name = "Helion",
+                        Address = "Kościuszki 1c, 44-100 Gliwice",
+                        YearFounded = 1991,
+                    },
+                    new Publisher()
+                    {
+                        PublisherId = 3,
+                        Name = "Insignis",	
+                        Address = "Lubicz 17d, 31-503 Kraków",
+                        YearFounded = 1991,
+                    }
+                );
+            //Books
+            modelBuilder.Entity<Book>().HasData(
+                new Book()
+                {
+                    BookId = 1,
+                    Title = "Cyberiada",
+                    Genre = "Fantastyka naukowa",
+                    PublicationYear = 1965,
+                    PublisherId = 1
+                },
+                new Book()
+                {
+                    BookId = 2,
+                    Title = "Niezwyciężony",
+                    Genre = "Fantastyka naukowa",
+                    PublicationYear = 1964,
+                    PublisherId = 1
+                },
+                 new Book()
+                 {
+                     BookId = 3,
+                     Title = "Eden",
+                     Genre = "Fantastyka naukowa",
+                     PublicationYear = 1958,
+                     PublisherId = 1
+                 },
+                 new Book()
+                 {
+                     BookId = 4,
+                     Title = "Wzorce projektowe",
+                     Genre = "Informatyka",
+                     PublicationYear = 1994,
+                     PublisherId = 2,
+                 },
+                 new Book()
+                 {
+                     BookId = 5,
+                     Title = "The Code Breaker",
+                     Genre = "Non-fiction",
+                     PublicationYear = 2021,
+                     PublisherId = 3,
+                 },
+                 new Book()
+                 {
+                     BookId = 6,
+                     Title = "Steve Jobs",
+                     Genre = "Biografia",
+                     PublicationYear = 2011,
+                     PublisherId = 3,
+                 },
+                 new Book()
+                 {
+                     BookId = 7,
+                     Title = "Czysty kod. Podręcznik dobrego programisty",
+                     Genre = "Informatyka",
+                     PublicationYear = 2008,
+                     PublisherId = 2,
+                 }
+            );
+            //Books_Authors
+            modelBuilder.Entity<Book_Author>().HasData(
+                new Book_Author()
+                {
+                    Id = 1,
+                    BookId = 1,//Cyberiada
+                    AuthorId = 1,//Lem
+                },
+                new Book_Author()
+                {
+                    Id = 2,
+                    BookId = 2,//Niezwyciężony
+                    AuthorId = 1,//Lem
+                },
+                new Book_Author()
+                {
+                    Id = 3,
+                    BookId = 3,//Eden
+                    AuthorId = 1,//Lem
+                },
+                new Book_Author()
+                {
+                    Id = 4,
+                    BookId = 4,//Wzorce projektowe
+                    AuthorId = 3,//Gamma
+                },
+                new Book_Author()
+                {
+                    Id = 4,
+                    BookId = 4,//Wzorce projektowe
+                    AuthorId = 4,//Helm
+                },
+                new Book_Author()
+                {
+                    Id = 4,
+                    BookId = 4,//Wzorce projektowe
+                    AuthorId = 5,//Johnson
+                },
+                new Book_Author()
+                {
+                    Id = 4,
+                    BookId = 4,//Wzorce projektowe
+                    AuthorId = 6,//Vlissides
+                },
+                new Book_Author()
+                {
+                    Id = 5,
+                    BookId = 5,//Code Breaker
+                    AuthorId = 7,//Isaacson
+                },
+                new Book_Author()
+                {
+                    Id = 6,
+                    BookId = 6,//S. Jobs
+                    AuthorId = 7,//Isaacson
+                },
+                new Book_Author()
+                {
+                    Id = 7,
+                    BookId = 7,//Czysty kod
+                    AuthorId = 2,//Martin
+                }
+            );
+        }
+    }
+}
---
+++ Migrations/20240116132340_m1.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataBaseComplete.Migrations
+{
+    /// <inheritdoc />
+    public partial class m1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CountryOfOrigin = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.AuthorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Publisher",
+                columns: table => new
+                {
+                    PublisherId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearFounded = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publisher", x => x.PublisherId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    ProviderKey = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LoginProvider = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublicationYear = table.Column<int>(type: "int", nullable: false),
+                    PublisherId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                    table.ForeignKey(
+                        name: "FK_Books_Publisher_PublisherId",
+                        column: x => x.PublisherId,
+                        principalTable: "Publisher",
+                        principalColumn: "PublisherId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book_Author",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book_Author", x => new { x.BookId, x.AuthorId });
+                    table.ForeignKey(
+                        name: "FK_Book_Author_Author_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Author",
+                        principalColumn: "AuthorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Book_Author_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Author",
+                columns: new[] { "AuthorId", "CountryOfOrigin", "DateOfBirth", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "Poland", new DateTime(1921, 9, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Stanisław", "Lem" },
+                    { 2, "America", new DateTime(1952, 12, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "Robert C.", "Martin" },
+                    { 3, "Switzerland", new DateTime(1961, 3, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "Erich", "Gamma" },
+                    { 4, "", new DateTime(9999, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Richard", "Helm" },
+                    { 5, "", new DateTime(9999, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ralph", "Johnson" },
+                    { 6, "America", new DateTime(1961, 8, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "John", "Vlissides" },
+                    { 7, "America", new DateTime(1952, 3, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "Walter", "Isaacson" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Publisher",
+                columns: new[] { "PublisherId", "Address", "Name", "YearFounded" },
+                values: new object[,]
+                {
+                    { 1, "Długa 1, 31-147 Kraków", "Wydawnictwo Literackie", 1953 },
+                    { 2, "Kościuszki 1c, 44-100 Gliwice", "Helion", 1991 },
+                    { 3, "Lubicz 17d, 31-503 Kraków", "Insignis", 1991 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "Genre", "PublicationYear", "PublisherId", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Fantastyka naukowa", 1965, 1, "Cyberiada" },
+                    { 2, "Fantastyka naukowa", 1964, 1, "Niezwyciężony" },
+                    { 3, "Fantastyka naukowa", 1958, 1, "Eden" },
+                    { 4, "Informatyka", 1994, 2, "Wzorce projektowe" },
+                    { 5, "Non-fiction", 2021, 3, "The Code Breaker" },
+                    { 6, "Biografia", 2011, 3, "Steve Jobs" },
+                    { 7, "Informatyka", 2008, 2, "Czysty kod. Podręcznik dobrego programisty" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Book_Author",
+                columns: new[] { "AuthorId", "BookId", "Id" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 1, 2, 2 },
+                    { 1, 3, 3 },
+                    { 3, 4, 4 },
+                    { 4, 4, 4 },
+                    { 5, 4, 4 },
+                    { 6, 4, 4 },
+                    { 7, 5, 5 },
+                    { 7, 6, 6 },
+                    { 2, 7, 7 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_Author_AuthorId",
+                table: "Book_Author",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_PublisherId",
+                table: "Books",
+                column: "PublisherId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Book_Author");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Publisher");
+        }
+    }
+}
